d3e722a07abe44d19e561b0fa93cdd7abca46424
Robson Gabriel <34819099+Kegure@users.noreply.github.com>
2024-09-16 10:22:00 -0300

    Add files via upload

---
+++ app/src/main/java/com/example/chrono_diary/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.chrono_diary
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(navController,"AuthScreen"){
+                        composable("AuthScreen") {
+                            authScreen(
+                                onSignInClick = { user ->
+                                    navController.navigate("UserScreen/${user}")
+                                },
+                                onSignUpClick = {
+                                    navController.navigate("SignUpScreen")
+                                }
+                            )
+                        }
+                        /*composable("SignUpScreen"){
+                            SignUpScreen(
+                                onSignUpClick = {
+                                    navController.popBackStack()
+                                }
+                            )
+                        }*/
+                        composable("UserScreen/{user}"){ entry ->
+                            entry.arguments?.getString("user")?.let { //user ->
+                                //UserScreen (onSignUpClick(user = user))
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
